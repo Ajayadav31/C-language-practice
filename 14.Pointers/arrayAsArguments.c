5da71f4e47788a2d89047d2f7954165fe396e9e6
Ajay <ajayadav3108@gmail.com>
2024-05-18 11:46:02 +0530

    Second Style

---
+++ 14.Pointers/arrayAsArguments.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void display(int []);
+int main()
+{
+    int arr[5];
+    int i;
+    for(i=0;i<5;i++)
+    {
+        printf("Enter elemnet no %d :",i+1);
+        scanf("%d",&arr[i]);
+    }
+    printf("Elemnets are :");
+    display(arr);
+    return 0;
+}
+void display(int p[])
+{
+    for(int i=0;i<5;i++)
+    {
+        printf("\n%d",*(p+i));
+    }
+}
