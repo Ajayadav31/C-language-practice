071c0a3c0b114986751b87836238f332c71b5144
Ajay <ajayadav3108@gmail.com>
2024-05-18 15:45:18 +0530

    using typedef in structure 2nd way

---
+++ 16.Structure/using_typedef.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+typedef struct Student
+{
+    int roll;
+    char grade;
+    float per;
+}Student;
+
+int main()
+{
+    Student s;
+    s.roll=12;
+    s.grade='A';
+    s.per=87;
+    printf("roll no is : %d\ngrade is : %c\npercentage is : %f",s.roll,s.grade,s.per);    
+    return 0;
+}
