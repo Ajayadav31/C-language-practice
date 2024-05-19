4eca01ab09bfbafc3bb5f1c275a9fbbfd2259cce
Ajay <ajayadav3108@gmail.com>
2024-05-19 23:10:13 +0530

    Basic code of do_while loop

---
+++ 9.loop/do_whileLoop.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int i=1;//initialization
+    do
+    {
+        printf("%d\n",i);
+        i++;//increament
+    }while(i<=10);//test_condition
+    return 0;
+}
