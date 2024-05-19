5d1b39c190ae7c83ca0d29481e7a72fbea5f869b
Ajay <ajayadav3108@gmail.com>
2024-05-19 23:07:56 +0530

    Basic code of while loop

---
+++ 9.loop/whileLoop.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int i=1;//initialization
+    while(i<=10)//test_condition
+    {
+        printf("%d\n",i);
+        i++;//increament
+    }
+    return 0;
+}
