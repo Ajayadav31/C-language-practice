bae9adea4da93cb78f51a29f56f674ccd5d0030c
Ajay <ajayadav3108@gmail.com>
2024-05-20 22:39:22 +0530

    Area of circle

---
+++ Assignments/assignment1/qus10.c
@@ -0,0 +1,10 @@
+//WAP to find area of circle
+#include<stdio.h>
+int main()
+{
+    float a;
+    printf("Enter radius : ");
+    scanf("%f",&a);
+    printf("area of circle is : %f",3.14*a*a);
+    return 0;
+}
