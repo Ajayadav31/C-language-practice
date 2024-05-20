636e974c47aee6c737a00816fa3914f23fe750f7
Ajay <ajayadav3108@gmail.com>
2024-05-20 22:37:28 +0530

    Square of a number

---
+++ Assignments/assignment1/qus9.c
@@ -0,0 +1,10 @@
+//WAP to calculate square of a number
+#include<stdio.h>
+int main()
+{
+    int a;
+    printf("Enter length and breagth : ");
+    scanf("%d",&a);
+    printf("Square is : %d",a*a);
+    return 0;
+}
