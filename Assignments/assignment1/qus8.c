60d58c3936a1939bb61a3003f5b2707673600d05
Ajay <ajayadav3108@gmail.com>
2024-05-20 22:35:44 +0530

    Area of rectangle

---
+++ Assignments/assignment1/qus8.c
@@ -0,0 +1,10 @@
+//WAP to calculate area of a rectangle 
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    printf("Enter length and breagth : ");
+    scanf("%d %d",&a,&b);
+    printf("Area of rectangle is : %d",a*b);
+    return 0;
+}
