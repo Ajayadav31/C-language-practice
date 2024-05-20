8693c6253e7ec9dd9ab8b44ce785b7e7f5ace89b
Ajay <ajayadav3108@gmail.com>
2024-05-20 22:25:48 +0530

    Printing backslash

---
+++ Assignments/assignment1/qus4.c
@@ -0,0 +1,8 @@
+//WAP to print \n on the screen
+#include<stdio.h>
+int main()
+{
+    printf("\\n");
+ //   printf("\\n\a"); //this tell you imp concept that what we write after \ compiler ignore its next char and display char which is next to next to \.
+     return 0;
+}
