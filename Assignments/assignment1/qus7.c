7537fc97108baa6eb0aa8c83d35f1d94b42e3f3e
Ajay <ajayadav3108@gmail.com>
2024-05-20 22:33:53 +0530

    Sum of two integers

---
+++ Assignments/assignment1/qus7.c
@@ -0,0 +1,10 @@
+//WAP to calculate sum of two integers.number are taken from the user through keyboard
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    printf("Enter two integers : ");
+    scanf("%d %d",&a,&b);
+    printf("Sum is : %d",a+b);
+    return 0;
+}
