e482fb526ee7c8a54ca474578d2dc617734ff556
Ajay <ajayadav3108@gmail.com>
2024-05-21 21:05:20 +0530

    Volume of cuboid

---
+++ Assignments/assignment2/qus4.c
@@ -0,0 +1,10 @@
+//WAP to calculate volume of a cuboid
+#include<stdio.h>
+int main()
+{
+    float l,w,h;
+    printf("Enter amount,rate and time : ");
+    scanf("%f %f %f",&l,&w,&h);
+    printf("Simple interest is : %f ",l*w*h);
+    return 0;
+}
