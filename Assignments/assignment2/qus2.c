adca0bdebec1694bdc516fe5a1123fe16311d18b
Ajay <ajayadav3108@gmail.com>
2024-05-21 20:59:02 +0530

    Circumference of circle

---
+++ Assignments/assignment2/qus2.c
@@ -0,0 +1,10 @@
+//WAP to calculate circumference of a circle
+#include<stdio.h>
+int main()
+{
+    float rad;
+    printf("Enter radius : ");
+    scanf("%f",&rad);
+    printf("Circumference is : %f",2*3.14*rad);
+    return 0;
+}
