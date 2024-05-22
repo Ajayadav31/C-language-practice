e5607e3bd60a0ea8eabf4222cf90d38ddb584869
Ajay <ajayadav3108@gmail.com>
2024-05-22 21:55:58 +0530

    ASCII code to char

---
+++ Assignments/assignment3/qus2.c
@@ -0,0 +1,10 @@
+//WAP to input a ASCII value from the user and print its chracter
+#include<stdio.h>
+int main()
+{
+    int a;
+    printf("Enter ASCII value : ");
+    scanf("%d",&a);
+    printf("Character is : %c",a);
+    return 0;
+}
