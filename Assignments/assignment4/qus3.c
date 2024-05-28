a4ed3b257dc87c0c0cba54fa6e1cf010819db9a2
AJAY YADAV <ajayadav3108@gmail.com>
2024-05-27 22:48:07 -0700

    Increamnet operator in char datatype

---
+++ Assignments/assignment4/qus3.c
@@ -0,0 +1,9 @@
+//WAP with one char type variable. assign "A" in the variable. now change the value of variable from 'A' to 'B' using increament operator
+#include<stdio.h>
+int main()
+{
+    char ch ='A';
+    ch++;
+    printf("%c",ch);
+    return 0;
+}
