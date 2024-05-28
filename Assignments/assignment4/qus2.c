9895008fdb299687feabc2799257abf6a2cafda5
AJAY YADAV <ajayadav3108@gmail.com>
2024-05-27 22:43:39 -0700

    size of real constant

---
+++ Assignments/assignment4/qus2.c
@@ -0,0 +1,9 @@
+//WAP to calculate size of a real constant
+#include<stdio.h>
+int main()
+{
+    int a=15;
+  //  printf("Size of A is : %d",sizeof(15));
+   printf("Size of A is : %d",sizeof(a));
+    return 0;
+}
