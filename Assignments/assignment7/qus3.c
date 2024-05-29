396decb4c696d2624a0d5eb360879f0d35b2f97b
AJAY YADAV <ajayadav3108@gmail.com>
2024-05-28 23:55:53 -0700

    Even or odd

---
+++ Assignments/assignment7/qus3.c
@@ -0,0 +1,13 @@
+//WAP to chech
+#include<stdio.h>
+int main()
+{
+    int x;
+    printf("Enter a number : ");
+    scanf("%d",&x);
+    if(x%2)
+        printf("odd");
+    else
+        printf("even");
+    return 0;
+}
