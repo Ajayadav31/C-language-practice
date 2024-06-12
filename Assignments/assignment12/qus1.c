ad58619c33e3cfea01d54555d6f858f21009a590
AJAY YADAV <ajayadav3108@gmail.com>
2024-06-12 09:46:14 -0700

    printing name n times by while loop

---
+++ Assignments/assignment12/qus1.c
@@ -0,0 +1,13 @@
+//WAP to print your name n times on the screen
+#include<stdio.h>
+int main()
+{
+    int i=0,n;
+    printf("Enter a no : ");
+    scanf("%d",&n);
+    while(i<n)
+    {
+        printf("Ajay\n");
+        i++;
+    }
+}
