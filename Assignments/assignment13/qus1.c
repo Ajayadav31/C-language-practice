aa8dfa970e8ee6664949040a113a4f42f0ebc0a4
AJAY YADAV <ajayadav3108@gmail.com>
2024-06-16 10:19:15 -0700

    sum of n no

---
+++ Assignments/assignment13/qus1.c
@@ -0,0 +1,14 @@
+//WAP to calculate sum of first N natural numbers
+#include<stdio.h>
+int main()
+{
+    int i,n,sum=0;
+    printf("Enter a no : ");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        sum=sum+i;
+    }
+    printf("Sum is : %d",sum);
+    return 0;
+}
