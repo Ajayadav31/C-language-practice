59faa3c2dd44a8595089c5584abefbfccacd4da2
AJAY YADAV <ajayadav3108@gmail.com>
2024-06-16 10:20:36 -0700

    sum of odd no

---
+++ Assignments/assignment13/qus3.c
@@ -0,0 +1,15 @@
+//WAP to calculate sum of first N natural odd numbers
+#include<stdio.h>
+int main()
+{
+    int i,n,sum=0;
+    printf("Enter a no : ");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        if(i%2!=0)
+            sum=sum+i;
+    }
+    printf("Sum is : %d",sum);
+    return 0;
+}
