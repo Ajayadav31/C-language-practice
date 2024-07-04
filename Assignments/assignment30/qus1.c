820cc1ffc02b28dea6ad0c584fafaf0751773266
AJAY YADAV <ajayadav3108@gmail.com>
2024-07-04 22:15:10 +0530

    Sorting of array in ascending order

---
+++ Assignments/assignment30/qus1.c
@@ -0,0 +1,29 @@
+//WAP to sort elements of an array of size 10. take array values from the user
+#include<stdio.h>
+int main()
+{
+    int arr[10];
+    int i,j,temp;
+    for(i=0;i<10;i++)
+    {
+        printf("enter element : ");
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<9;i++)
+    {
+        for(j=i+1;j<10;j++)
+        {
+            if(arr[i]>arr[j])
+            {
+                int temp=arr[i];
+                arr[i]=arr[j];
+                arr[j]=temp;
+            }
+        }
+    }
+    for(i=0;i<10;i++)
+    {
+        printf("%d ",arr[i]);
+    }
+    return 0;
+}
