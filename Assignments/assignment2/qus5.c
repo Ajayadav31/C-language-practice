06405a6f14872390303044398447b6dc36c357f3
AJAY YADAV <ajayadav3108@gmail.com>
2024-12-14 14:11:17 +0530

    Fixed logic issue

--- Assignments/assignment2/qus5.c
+++ Assignments/assignment2/qus5.c
@@ -1,13 +1,22 @@
 //WAP to ask the about the cost price and selling price banana per dozen. calculate the profit or loss earned upon selling 25 bananas
-#include<stdio.h>
-int main()
-{
-    int cost_p,selling_p;
-    printf("Enter cost price and selling price: ");
-    scanf("%d %d",&cost_p,&selling_p);
-    if(cost_p<selling_p)
-        printf("profit is : %f",((float)25/12)*selling_p);
-    else
-        printf("loss is : %f",((float)25/12)*selling_p);
+#include <stdio.h>
+
+int main() {
+    int cost_p, selling_p;
+    printf("Enter cost price and selling price per dozen: ");
+    scanf("%d %d", &cost_p, &selling_p);
+
+    // Cost and selling price for 25 bananas
+    float cost_for_25 = (25.0 / 12) * cost_p;
+    float selling_for_25 = (25.0 / 12) * selling_p;
+
+    if (cost_for_25 < selling_for_25) {
+        printf("Profit is: %.2f\n", selling_for_25 - cost_for_25);
+    } else if (cost_for_25 > selling_for_25) {
+        printf("Loss is: %.2f\n", cost_for_25 - selling_for_25);
+    } else {
+        printf("No profit, no loss.\n");
+    }
+
     return 0;
 }
